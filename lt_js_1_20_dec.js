59f3940e7198b5ebbcd3fbed258d4a98e891ed4d
StevenAcan <stevenacan@outlook.com>
2025-12-20 10:26:33 +0800

    20-Dec-2025, 10:26, Home, Mac Studio

---
+++ lt_js_1_20_dec.js
@@ -0,0 +1,15 @@
+/**
+ * @file lt_js_1_20_dec.js
+ * @description
+ * Long term studying of JavaScript
+ * Resource: https://javascript.info/
+ * Topic: An introduction to JavaScript
+ * Where At: "Code editors" https://javascript.info/code-editors
+ * @author StevenAcan <stevenacan@outlook.com>
+ * @created 2025-12-20
+ * @modified 2025-12-20
+ * @version 1.0.0
+ * @license e.g ( MIT (SPDX: MIT) )
+ */
+
+"use strict";
