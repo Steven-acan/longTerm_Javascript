325c8d7683ff660f0628972d9bbac0186c17cd08
StevenAcan <stevenacan@outlook.com>
2025-12-24 21:34:20 +0800

    24-Dec-2025-21-34-Wednesday, Home

---
+++ lt_js_3_24_Dec_2025.js
@@ -0,0 +1,40 @@
+/**
+ * @file lt_js_3_24_Dec_2025.js
+ * @description
+ * Long term studying of JavaScript
+ * Resource: https://javascript.info/
+ * Topic: An introduction to JavaScript
+ * Where At: "BigInt", https://javascript.info/types
+ * @author StevenAcan <stevenacan@outlook.com>
+ * @created 2025-12-24
+ * @modified 2025-12-24
+ * @version 1.0.0
+ * @license e.g ( MIT (SPDX: MIT) )
+ */
+
+/* Study Notes:
+
+1. 
+*/
+
+"use strict";
+
+let myFirtBigInt = 1234567890123456789012345678901234567890n;
+
+console.log("My First BigInt:", myFirtBigInt);
+
+let mySecondBigInt = 323n;
+
+console.log("My Second BigInt:", mySecondBigInt);
+
+let myString1 = "StevenAcan";
+
+console.log("My String 1:", myString1);
+
+let myString2 = `Name is ${myString1}`;
+
+console.log("My String 2:", myString2);
+
+console.log(myString1 + " - " + myString2);
+
+console.log(typeof Math);
