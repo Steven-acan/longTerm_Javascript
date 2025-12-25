759590dfe4d41b3f1bccfeccd08ba9029dc47e87
StevenAcan <stevenacan@outlook.com>
2025-12-25 22:13:38 +0800

    25-Dec-2025-22-13-Thursday, Home

---
+++ lt_js_4_25_Dec_2025.js
@@ -0,0 +1,49 @@
+/**
+ * @file lt_js_4_25_Dec_2025.js
+ * @description
+ * Long term studying of JavaScript
+ * Resource: https://javascript.info/
+ * Topic: An introduction to JavaScript
+ * Where At: https://youtu.be/Oe421EPjeBE?si=Vti_miGF0h9T0Eri
+ *           start with this courses, will study by project based learning
+ * @author StevenAcan <stevenacan@outlook.com>
+ * @created 2025-12-25
+ * @modified 2025-12-25
+ * @version 1.0.0
+ * @license e.g ( MIT (SPDX: MIT) )
+ */
+
+/* Study Notes:
+    *** Type Conversions
+
+
+*/
+
+"use strict";
+
+console.log(typeof String());
+console.log(typeof String);
+console.log(String());
+if (String() === "") {
+  console.log("Empty String");
+}
+
+let myName = "StevenAcan";
+let myAge = Number(myName);
+console.log(myAge);
+
+if (myAge != NaN) {
+  console.log("NaN is NaN");
+}
+
+let myHight;
+console.log(myHight);
+
+myHight = null;
+console.log(myHight);
+
+let myBlankString = "";
+
+if (myBlankString != myHight) {
+  console.log("Blank String is not equal to null");
+}
